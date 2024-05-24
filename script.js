9104ddeb2f4cb629cbd5058d3142d175d70beae4
OWNER OF CODING SUFYAN <161226014+CODEWITHSUFYAN@users.noreply.github.com>
2024-05-24 04:41:33 -0700

    chapter 1 task done

---
+++ script.js
@@ -0,0 +1,13 @@
+alert('Error! Please enter a valid password.')
+
+alert('Welcome to JS Land... ' )
+ 
+alert('Happy Coding!')
+
+alert('Welcome to JS Land... ' )
+
+alert('Happy Coding!')
+
+alert('Prevent this page from creating additional dialogs.')
+
+alert('Hello...I can run JS through my web browser console')
